429a49404bd67dfa6c6227049d019b75d19ba274
marku <markus.hauschild.negativ@googlemail.com>
2018-04-18 16:21:21 +0200

    Add global variables and bind View objects

--- app/src/main/java/com/mahausch/textme/MainActivity.java
+++ app/src/main/java/com/mahausch/textme/MainActivity.java
@@ -2,12 +2,39 @@ package com.mahausch.textme;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MessageAdapter messageAdapter;
+
+    @BindView(R.id.recyclerView)
+    private RecyclerView recyclerView;
+
+    @BindView(R.id.progressBar)
+    private ProgressBar progressBar;
+
+    @BindView(R.id.photoPickerButton)
+    private ImageButton photoPickerButton;
+
+    @BindView(R.id.messageEditText)
+    private EditText messageEditText;
+
+    @BindView(R.id.sendButton)
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
