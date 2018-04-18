a9869f03e7c21a50b0aad7eaa93e0177a169c900
marku <markus.hauschild.negativ@googlemail.com>
2018-04-18 16:49:26 +0200

    Add MessageAdapter with implementation

---
+++ app/src/main/java/com/mahausch/textme/MessageAdapter.java
@@ -0,0 +1,79 @@
+package com.mahausch.textme;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+
+    private Context context;
+    private List<TextMeMessage> messageList;
+
+
+    public MessageAdapter(Context context, List<TextMeMessage> objects) {
+        this.context = context;
+        this.messageList = objects;
+    }
+
+    @Override
+    public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_message, parent, false);
+        return new MessageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MessageViewHolder holder, int position) {
+        TextMeMessage message = messageList.get(position);
+
+        boolean isPhoto = message.getPhotoUrl() != null;
+        if (isPhoto) {
+            holder.messageTextView.setVisibility(View.GONE);
+            holder.photoImageView.setVisibility(View.VISIBLE);
+            Glide.with(holder.photoImageView.getContext())
+                    .load(message.getPhotoUrl())
+                    .into(holder.photoImageView);
+        } else {
+            holder.messageTextView.setVisibility(View.VISIBLE);
+            holder.photoImageView.setVisibility(View.GONE);
+            holder.messageTextView.setText(message.getText());
+        }
+        holder.nameTextView.setText(message.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    class MessageViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.photoImageView)
+        ImageView photoImageView;
+
+        @BindView(R.id.messageTextView)
+        TextView messageTextView;
+
+        @BindView(R.id.nameTextView)
+        TextView nameTextView;
+
+        public MessageViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+    }
+}
